050b2ef39e9b2058726215f43dde75d6d246c449
Tukaram Gore <goretukaram62@gmail.com>
2025-04-13 12:11:00 +0530

    Revert "AI Module"

--- app/utils/predictor.py
+++
@@ -1,8 +0,0 @@
-import joblib
-import numpy as np
-
-model = joblib.load('app/models/career_predictor.pkl')
-
-def predict_career(data):
-    prediction = model.predict([np.array(data)])
-    return prediction[0]
